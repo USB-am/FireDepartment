8904688cd8bb54673d7a6b3d5f41c83c6f49aa77
Цупко Роман Игоревич <CupkoRI@azniirkh.local>
2022-04-25 12:30:08 +0300

    25_04_2022__12_30: Finished PhoneField

--- UI/fields/phone_field.py
+++ UI/fields/phone_field.py
@@ -15,22 +15,28 @@ path_to_kv_file = os.path.join(
 Builder.load_file(path_to_kv_file)
 
 
+_PHONE_NUMBER_PATTERN = r'\+?[78](\d{3})(\d{3})(\d{2})(\d{2})'
+_REPL = r'+7 (\1) \2-\3-\4'
+
+
 class PhoneField(MDTextField):
 	def __init__(self, title: str):
 		self.title = title.title()
 		self.info_text = LANG.get(self.title, '')
-		# print(Settings.FONT_COLOR)
 
 		super().__init__()
 
 	def insert_text(self, substring: str, from_undo: bool=False) -> None:
 		super().insert_text(substring, from_undo)
-		# print(self.text)
-		numbers = ''.join(re.findall(r'\d', self.text))
-		if len(numbers) == 11:
-			self.text = self.numbers_to_phone(numbers)
-		print(self.text)
+
+		update_text = self.numbers_to_phone(self.text)
+		self.text = update_text
 
 	@staticmethod
-	def numbers_to_phone(numbers) -> str:
-		return '8 (800) 555 35 35'
+	def numbers_to_phone(numbers: str) -> str:
+		only_numbers = ''.join(re.findall(r'\d', numbers))
+		if len(only_numbers) == 11:
+			result = re.sub(_PHONE_NUMBER_PATTERN, _REPL, only_numbers)
+			return result
+
+		return numbers
