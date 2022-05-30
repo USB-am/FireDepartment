c205102a6497d9190956e9c66cd3ec57c084b0be
Цупко Роман Игоревич <CupkoRI@azniirkh.local>
2022-05-30 17:00:28 +0300

    30_05_2022__17_00: Start creating filters to MainPage

--- app/tools/fields/search_block.py
+++ app/tools/fields/search_block.py
@@ -6,6 +6,8 @@ from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 
 from config import PATTERNS_DIR
+from app.tools.custom_widgets import FDTextField
+from app.tools.custom_widgets.button import FDIconButton
 
 
 path_to_kv_file = os.path.join(PATTERNS_DIR, 'fields', 'search_block.kv')
@@ -13,4 +15,10 @@ Builder.load_file(path_to_kv_file)
 
 
 class SearchBlock(MDBoxLayout):
-	pass
+	@property
+	def text_field(self) -> FDTextField:
+		return self.ids.text_field
+
+	@property
+	def search_button(self) -> FDIconButton:
+		return self.ids.search_button
