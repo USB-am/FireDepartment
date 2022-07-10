1f4a9d8a3a0736edc516cbfd055ac2b6e761698b
USB-am <smit.acer@mail.ru>
2022-07-10 23:48:45 +0300

    10_07_2022__23_49: Rewrite Dialog content block

--- app/screens/emergency.py
+++ app/screens/emergency.py
@@ -4,7 +4,7 @@ import os
 
 from kivy.lang import Builder
 
-from config import PATTERNS_DIR
+from config import PATTERNS_DIR, LOCALIZED
 from data_base import Emergency
 from app.tools.custom_widgets import CustomScreen
 from app.tools.fields import ToManyDisplayField
@@ -23,8 +23,15 @@ class EmergencyPage(CustomScreen):
 		content.clear_widgets()
 
 		for title, value in values.items():
-			if title == 'humans':
-				w = ToManyDisplayField(title)
-				w.update(value)
-
-				content.add_widget(w)
+			if title == 'title':
+				print(LOCALIZED.translate(title), value)
+			elif title == 'discription':
+				print(LOCALIZED.translate(title), value)
+			elif title == 'urgent':
+				print(LOCALIZED.translate(title), '!' if value else '.')
+			elif title == 'humans':
+				field = ToManyDisplayField(title, value)
+				field.update()
+				content.add_widget(field)
+			elif title == 'tags':
+				print(LOCALIZED.translate(title), list(map(str, value)))
