fcff1b537fd29a4a207ee14b60861a49fde2a081
Цупко Роман Игоревич <CupkoRI@azniirkh.local>
2022-08-22 17:12:24 +0300

    22_08_2022__17_15: Create dialog window and concat with human list-element

--- uix/__init__.py
+++ uix/__init__.py
@@ -2,7 +2,17 @@ from .scroll_frame import FDScrollFrame
 from .search_block import FDSearchBlock
 from .elements_list import FDExpansionPanel, ExpansionEmergencyElement, ExpansionOptionsElement
 from .notebook import FDNoteBook, FDEmergencyTab
+from .dialog import FDDialog, HumanDialogContent
 
 
-__all__ = ('FDScrollFrame', 'FDSearchBlock', 'FDExpansionPanel', 'ExpansionEmergencyElement',
-	'ExpansionOptionsElement', 'FDNoteBook', 'FDEmergencyTab',)
+__all__ = (
+	'FDScrollFrame',
+	'FDSearchBlock',
+	'FDExpansionPanel',
+	'ExpansionEmergencyElement',
+	'ExpansionOptionsElement',
+	'FDNoteBook',
+	'FDEmergencyTab',
+	'FDDialog',
+	'HumanDialogContent',
+	)
