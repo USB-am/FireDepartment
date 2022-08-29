4cc6dcfe58d4f2a5e46961d5a3e4b649151e9cf9
USB-am <smit.acer@mail.ru>
2022-08-29 23:37:52 +0300

    29_08_2022__23_37: Divided the post editing class into concrete classes

--- run.py
+++ run.py
@@ -308,27 +308,105 @@ class EditEntryList(CustomScrolledScreen):
 			self.add_widgets(element)
 
 
-class EditEntry(CustomScrolledScreen):
-	''' Базовый класс редактирования записи базы данных '''
+# class EditEntry(CustomScrolledScreen):
+# 	''' Базовый класс редактирования записи базы данных '''
 
-	def __init__(self, path_manager: PathManager, screen_name: str):
-		super().__init__()
+# 	def __init__(self, path_manager: PathManager, screen_name: str):
+# 		super().__init__()
 
-		self.name = screen_name
-		self.path_manager = path_manager
+# 		self.name = screen_name
+# 		self.path_manager = path_manager
 
+# 		self.element = None
+
+# 		self.setup()
+# 		self.fill_content()
+
+# 	def setup(self) -> None:
+# 		self.toolbar.title = LOCALIZED.translate(f'Edit {self.name}')
+# 		self.toolbar.add_left_button('arrow-left', lambda e: self.path_manager.back())
+# 		self.toolbar.add_right_button('delete', lambda e: print('Delete this entry'))
+
+# 	def fill_content(self) -> None:
+# 		pass
+
+# 	def fill_fields(self, element: db.Model) -> None:
+# 		self.element = element
+
+
+class EditEntryTag(CreateEntryTag):
+	''' Экран редаектирования тега '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_tag'
 		self.element = None
 
-		self.setup()
-		self.fill_content()
+	def fill_fields(self, element: db.Model) -> None:
+		self.element = element
 
-	def setup(self) -> None:
-		self.toolbar.title = LOCALIZED.translate(f'Edit {self.name}')
-		self.toolbar.add_left_button('arrow-left', lambda e: self.path_manager.back())
-		self.toolbar.add_right_button('delete', lambda e: print('Delete this entry'))
 
-	def fill_content(self) -> None:
-		pass
+class EditEntryRank(CreateEntryRank):
+	''' Экран редаектирования звания '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_rank'
+		self.element = None
+
+	def fill_fields(self, element: db.Model) -> None:
+		self.element = element
+
+
+class EditEntryPosition(CreateEntryPosition):
+	''' Экран редаектирования должности '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_position'
+		self.element = None
+
+	def fill_fields(self, element: db.Model) -> None:
+		self.element = element
+
+
+class EditEntryHuman(CreateEntryHuman):
+	''' Экран редаектирования человека '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_human'
+		self.element = None
+
+	def fill_fields(self, element: db.Model) -> None:
+		self.element = element
+
+
+class EditEntryEmergency(CreateEntryEmergency):
+	''' Экран редаектирования ЧС '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_emergency'
+		self.element = None
+
+	def fill_fields(self, element: db.Model) -> None:
+		self.element = element
+
+
+class EditEntryWorktype(CreateEntryTag):
+	''' Экран редаектирования графика работы '''
+
+	def __init__(self, path_manager: PathManager):
+		super().__init__(path_manager)
+		
+		self.name = 'edit_worktype'
+		self.element = None
 
 	def fill_fields(self, element: db.Model) -> None:
 		self.element = element
@@ -363,6 +441,8 @@ class Application(MDApp):
 		self.edit_human_list = EditEntryList(self.path_manager, Human)
 		self.edit_emergency_list = EditEntryList(self.path_manager, Emergency)
 
+		# self.screen_manager.add_widget(EditEntryTag(self.path_manager))
+
 		self.screen_manager.add_widget(self.main_page)
 		self.screen_manager.add_widget(self.current_calls)
 		self.screen_manager.add_widget(self.options)
@@ -373,10 +453,13 @@ class Application(MDApp):
 		self.screen_manager.add_widget(self.edit_human_list)
 		self.screen_manager.add_widget(self.edit_emergency_list)
 
-		for table in (Tag, Rank, Position, Human, Emergency, Worktype):
-			name = f'edit_{table.__tablename__}'.lower()
-			screen = EditEntry(self.path_manager, name)
-			self.screen_manager.add_widget(screen)
+		for edit_screen in (EditEntryTag, EditEntryRank, EditEntryPosition, EditEntryHuman, EditEntryEmergency, EditEntryWorktype):
+			self.screen_manager.add_widget(edit_screen(self.path_manager))
+
+		# for table in (Rank, Position, Human, Emergency, Worktype):
+		# 	name = f'edit_{table.__tablename__}'.lower()
+		# 	screen = EditEntry(self.path_manager, name)
+		# 	self.screen_manager.add_widget(screen)
 
 	def build(self) -> ScreenManager:
 		return self.screen_manager
