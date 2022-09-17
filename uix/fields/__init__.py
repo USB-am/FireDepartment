2229fa83619399b3bbe779aa3568fc319d68b32a
USB-am <smit.acer@mail.ru>
2022-09-17 12:34:55 +0300

    17_09_2022__12_35: Udate DropDown field and EditColorTheme screen

--- uix/fields/__init__.py
+++ uix/fields/__init__.py
@@ -2,7 +2,7 @@ from .text_fields import StringField, PhoneField, DescriptionField, IntegerField
 from .boolean_field import BooleanField
 from .selected_list import SelectedList
 from .date_field import DateField, DateTimeField
-from .drop_down import DropDown, get_items
+from .drop_down import DropDown, gen_hue_items, gen_color_items
 
 
 __all__ = ('StringField', 'PhoneField', 'DescriptionField', 'IntegerField', 'BooleanField', 
