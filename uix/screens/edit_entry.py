3d3efb87092d6e95e2da7ad5244a79a3d3f2541f
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-09-28 11:59:05 +0300

    28_09_2022__12_00: Fix create rank method

--- uix/screens/edit_entry.py
+++ uix/screens/edit_entry.py
@@ -108,7 +108,7 @@ class EditEntryHuman(CreateEntryHuman):
 		self.toolbar.title = LOCALIZED.translate(self.name)
 		self.element = None
 
-		self.bind(on_pre_enter=lambda e: super().update_selected_lists())
+		# self.bind(on_pre_enter=lambda e: super().update_selected_lists())
 
 	def set_element(self, element: db.Model) -> None:
 		self.element = element
