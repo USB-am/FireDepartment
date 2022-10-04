1fa5e6df17803e4c86c78ad607fdd361ec257774
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-10-04 16:53:10 +0300

    04_10_2022__16_55: Modified test polygon WorkersFilter

--- temp/test_worktype_filter.py
+++ temp/test_worktype_filter.py
@@ -51,22 +51,69 @@ def init_humans() -> list:
 
 @dataclass
 class Filter():
+	''' Фильтр работников '''
 	humans: list
+
+	'''
+	TODO:
+		1. Сделать смещение графика работы относительно рабочего дня работника;
+		2. Найти диапазон недели, в которую будет входить необходимая дата;
+		3. Из диапазона недели определить, является ли день рабочим;
+		4. Из дня (п.3) сделать выборку по времени работы
+	'''
 
 	def get_on_date(self, datetime_: datetime) -> list:
 		output = [human for human in self.humans if self._is_works(datetime_, human)]
 
 		return output
 
-	def _is_works(self, date_: datetime, human: Human) -> bool:
+	def _is_works(self, datetime_: datetime, human: Human) -> bool:
+		work_day = human.work_day
+		wt = human.worktype	# Worktype.query.get(human.worktype)
+
+		week_bias = self.__calc_week_bias(wt, work_day)
+		now_week = self.__find_now_week(week_bias, datetime_)
+
 		return int(human.title[-1]) % 2
+
+	def __calc_week_bias(self, work_type: Worktype, work_day: datetime) -> tuple:
+		swd = work_type.start_work_day
+		fwd = work_type.finish_work_day
+		week_length = work_type.work_day_range + work_type.week_day_range
+		wd_count = work_day.toordinal()
+
+		out_swd_count = swd + timedelta(days=wd_count - swd.toordinal())
+		out_fwd_count = out_swd_count + timedelta(days=week_length)
+
+		return (out_swd_count, out_fwd_count)
+
+	def __find_now_week(self, week_range: tuple, search_day: datetime) -> tuple:
+		swd, fwd = week_range
+		swd_count = swd.toordinal()
+		fwd_count = fwd.toordinal()
+		day_count = search_day.toordinal()
+		week_length = fwd_count - swd_count
+
+		out_day_count = swd_count + day_count % week_length
+
+		return week_range
+
+	def __is_work_day(self, week_range: tuple, graph: tuple, day: datetime) -> bool:
+		''' Arguments:
+		`week_range: tuple[datetime, datetime]
+		`graph: tuple[int, int]
+			graph[0] - work_day_range
+			graph[1] - week_day_range
+		`day: datetime'''
+
+		return True
 
 
 def main():
 	humans = list(init_humans())
 	filter_ = Filter(humans)
 	today_workers = filter_.get_on_date(datetime.now())
-	[print(worker) for worker in today_workers]
+	# [print(worker) for worker in today_workers]
 
 
 if __name__ == '__main__':
