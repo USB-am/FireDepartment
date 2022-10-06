0c903ef8a71e50f65ae52e7bfd5d6e35b58055b3
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-10-06 16:52:00 +0300

    06_10_2022__16_55: Modified Notebook. Create close button

--- uix/screens/current_calls.py
+++ uix/screens/current_calls.py
@@ -21,7 +21,7 @@ class CurrentCalls(CustomScreen):
 	def setup(self) -> None:
 		self.toolbar.title = LOCALIZED.translate('Current calls')
 		self.toolbar.add_left_button('arrow-left', lambda e: self.path_manager.back())
-		self.toolbar.add_right_button('check-outline', lambda e: print('Done'))
+		self.toolbar.add_right_button('check-outline', lambda e: self.close_tab())
 
 	def fill_content(self) -> None:
 		self.notebook = FDNoteBook()
@@ -29,3 +29,12 @@ class CurrentCalls(CustomScreen):
 
 	def add_tab(self, element: db.Model) -> None:
 		self.notebook.add_widget(FDEmergencyTab(element))
+		self.notebook.switch_tab(self.notebook.get_tab_list()[-1])
+
+	def close_tab(self) -> None:
+		if len(self.notebook.get_tab_list()) > 1:
+			current_tab = self.notebook.current_tab
+			self.notebook.remove_widget(current_tab)
+
+		else:
+			print(':c')
