7493fd413fa689649dcd5c48a58cf4daeab36e10
USB-am <smit.acer@mail.ru>
2022-10-15 21:12:51 +0300

    15_10_2022__21_13: Create SelectionList and concat with EditList screen

--- uix/screens/edit_entry_list.py
+++ uix/screens/edit_entry_list.py
@@ -1,7 +1,8 @@
 from custom_screen import CustomScrolledScreen
 from data_base import db
 from config import LOCALIZED
-from uix import ExpansionEditListElement
+# from uix import ExpansionEditListElement
+from uix.fields.selection_list import SelectionList, EditSelectionElement
 
 
 class EditEntryList(CustomScrolledScreen):
@@ -13,6 +14,7 @@ class EditEntryList(CustomScrolledScreen):
 		self.name = f'edit_{table.__tablename__}_list'.lower()
 		self.path_manager = path_manager
 		self.table = table
+		# self.selection_list = MDSelectionList()
 
 		self.setup()
 		self.bind(on_pre_enter=lambda e: self.fill_content())
@@ -20,13 +22,20 @@ class EditEntryList(CustomScrolledScreen):
 	def setup(self) -> None:
 		self.toolbar.title = LOCALIZED.translate(f'Edit {self.name} list')
 		self.toolbar.add_left_button('arrow-left', lambda e: self.path_manager.back())
+		self.toolbar.add_right_button('delete', lambda e: print('Delete method'))
+
+		self.selection_list = SelectionList()
+		self.add_widgets(self.selection_list)
 
 	def fill_content(self) -> None:
-		self.clear()
+		self.selection_list.clear_widgets()
 		values = self.table.query.all()
 		sorted_values = sorted(values, key=lambda val: val.title)
 
 		for db_entry in sorted_values:
-			element = ExpansionEditListElement(db_entry)
-			element.binding(self.path_manager)
-			self.add_widgets(element)
+			self.selection_list.add_widget(EditSelectionElement(db_entry))
+
+		# for db_entry in sorted_values:
+		# 	element = ExpansionEditListElement(db_entry)
+		# 	element.binding(self.path_manager)
+		# 	self.add_widgets(element)
