75dd1e58da557d9b3226933e6a2a5ee584709d09
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-10-20 10:42:39 +0300

    20_10_2022__10_45: Added test for DateField

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,4 +1,9 @@
 import unittest
+import datetime
+
+from kivy.config import Config
+Config.set('graphics', 'width', '1')
+Config.set('graphics', 'height', '1')
 
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -26,13 +31,14 @@ APP = ApplicationForTests()
 class TestBooleanField(unittest.TestCase):
 	''' Тесты для BooleanField '''
 
+	@classmethod
 	def setUpClass(self):
 		self.widget = fields.BooleanField(icon='test', title='Test')
 		APP.get_screen().add_widget(self.widget)
 
+	@classmethod
 	def tearDownClass(self):
 		APP.get_screen().clear_widgets()
-		print('Clear all widgets')
 
 	def test_get_value(self):
 		self.assertEqual(self.widget.get_value(), False)
@@ -40,6 +46,30 @@ class TestBooleanField(unittest.TestCase):
 	def test_set_value(self):
 		self.widget.set_value(True)
 		self.assertEqual(self.widget.get_value(), True)
+
+
+class TestDateField(unittest.TestCase):
+	''' Тесты для виджета выбора даты '''
+
+	@classmethod
+	def setUpClass(self):
+		self.widget = fields.DateField(icon='test', title='Test')
+		APP.get_screen().add_widget(self.widget)
+
+	@classmethod
+	def tearDownClass(self):
+		APP.get_screen().clear_widgets()
+
+	def test_get_value(self):
+		self.assertEqual(self.widget.get_value(), None)
+		self.assertEqual(self.widget.ids.button.text, 'dd.mm.yyyy')
+
+	def test_set_value(self):
+		set_date = datetime.datetime.now().date()
+		string_date = set_date.strftime('%d.%m.%Y')
+		self.widget.set_value(set_date)
+		self.assertEqual(self.widget.get_value(), set_date)
+		self.assertEqual(self.widget.ids.button.text, string_date)
 
 
 if __name__ == '__main__':
