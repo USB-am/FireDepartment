d2be7a8df542e8a89d04ce6e4da72ddda6c32670
USB-am <smit.acer@mail.ru>
2023-02-23 17:12:53 +0300

    Concat SelectListItem with FDSelectList

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '650')
 
 
+from data_base import db, base_records
 from app import Application
 
 
 if __name__ == '__main__':
+	db.create_all()
+	base_records.write_records()
+
 	app = Application()
 	app.run()
