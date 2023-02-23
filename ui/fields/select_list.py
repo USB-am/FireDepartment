e4b78ffb2daede18c6302271200bb25f15ddfc54
USB-am <smit.acer@mail.ru>
2023-02-23 18:54:08 +0300

    Added get_value and set_value methods to FDSelectList

--- ui/fields/select_list.py
+++ ui/fields/select_list.py
@@ -13,10 +13,19 @@ Builder.load_file(paths.SELECTED_LIST_FIELD)
 class _SelectListItem(MDBoxLayout):
 	''' Элемент списка FDSelectList '''
 
-	def __init__(self, db_entry: db.Model):
+	def __init__(self, db_entry: db.Model, group: str=None):
 		self.db_entry = db_entry
+		self.group = group
 
 		super().__init__()
+
+	@property
+	def checkbox(self) -> bool:
+		return self.ids.checkbox.active
+
+	@checkbox.setter
+	def checkbox(self, value: bool) -> None:
+		self.ids.checkbox.active = value
 
 
 class FDSelectList(MDBoxLayout):
@@ -27,5 +36,21 @@ class FDSelectList(MDBoxLayout):
 	group = StringProperty(None)
 
 	def add(self, row_data: db.Model) -> None:
-		element = _SelectListItem(row_data)
+		element = _SelectListItem(row_data, group=self.group)
 		self.ids.lst.add_widget(element)
+
+	def get_value(self) -> list:
+		container = self.ids.lst
+		values = [child.db_entry for child in container.children \
+			if child.checkbox]
+
+		return values
+
+	def set_value(self, values: list) -> None:
+		container = self.ids.lst
+
+		for child in container.children:
+			child.checkbox = False
+
+			if child.db_entry in values:
+				child.checkbox = True
