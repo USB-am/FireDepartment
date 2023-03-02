88619f8ae8ba93a441cffb6e8a2038c1512997c9
USB-am <smit.acer@mail.ru>
2023-03-03 00:08:50 +0300

    Created ListItem for emergecies and created EmergencyContent layout

--- ui/frames/bottom_navigation_items.py
+++ ui/frames/bottom_navigation_items.py
@@ -2,9 +2,8 @@ from kivy.lang.builder import Builder
 from kivymd.uix.bottomnavigation import MDBottomNavigationItem
 
 from config import paths
-
-
-from kivymd.uix.label import MDLabel
+from .list_items import FDEmergencyListItem
+import data_base
 
 
 Builder.load_file(paths.BOTTOM_NAVIGATION_ITEMS)
@@ -16,5 +15,15 @@ class EmergenciesNavigationItem(MDBottomNavigationItem):
 	def __init__(self, **options):
 		super().__init__(**options)
 
-		[self.ids.content.add_widget(MDLabel(text=f'Row #{i+1}')) \
-			for i in range(100)]
+		self.__fill_content()
+
+	def __fill_content(self) -> None:
+		emergencies = data_base.Emergency.query.all()
+
+		self.add_list_items(*emergencies)
+
+	def add_list_items(self, *emergencies: data_base.Emergency) -> None:
+		container = self.ids.content
+
+		for emergency in emergencies:
+			container.add_widget(FDEmergencyListItem(model=emergency))
