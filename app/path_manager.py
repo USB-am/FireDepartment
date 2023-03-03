04297ba0f077385e93a3a9f3cec728eac1ce5fcb
USB-am <smit.acer@mail.ru>
2023-03-04 00:22:13 +0300

    Added singletone to PathManager

--- app/path_manager.py
+++ app/path_manager.py
@@ -4,9 +4,21 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class PathManager:
 	''' Менеджер путей '''
 
+	_instance = None
+
+	def __new__(cls, *args):
+		if cls._instance is None:
+			cls._instance = super(PathManager, cls).__new__(cls)
+
+		return cls._instance
+
 	def __init__(self, screen_manager: ScreenManager):
-		self.__screen_manager = screen_manager
+		if screen_manager is None:
+			self.__screen_manager = self._instance.__screen_manager
+		else:
+			self.__screen_manager = screen_manager
 		self._path = ['main',]
+		print(f'self._instance = {self._instance}\n{self}')
 
 	def forward(self, screen_name: str) -> Screen:
 		self.__screen_manager.current = screen_name
