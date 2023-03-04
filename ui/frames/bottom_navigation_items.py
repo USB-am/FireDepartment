1cba069a57ac6afdcef68306f036a8c08b283b9b
USB-am <smit.acer@mail.ru>
2023-03-05 02:15:47 +0300

    Added view for ContactsNavigationItem content

--- ui/frames/bottom_navigation_items.py
+++ ui/frames/bottom_navigation_items.py
@@ -2,7 +2,7 @@ from kivy.lang.builder import Builder
 from kivymd.uix.bottomnavigation import MDBottomNavigationItem
 
 from config import paths
-from .list_items import FDEmergencyListItem
+from .list_items import FDEmergencyListItem, FDHumanListItem
 import data_base
 
 
@@ -27,3 +27,23 @@ class EmergenciesNavigationItem(MDBottomNavigationItem):
 
 		[container.add_widget(FDEmergencyListItem(model=emergency)) \
 			for emergency in emergencies]
+
+
+class ContactsNavigationItem(MDBottomNavigationItem):
+	''' Ячейка с списком контактов '''
+
+	def __init__(self, **options):
+		super().__init__(**options)
+
+		self.__fill_content()
+
+	def __fill_content(self) -> None:
+		humans = data_base.Human.query.filter(data_base.Human.is_firefigher==True)
+
+		self.add_list_items(*humans)
+
+	def add_list_items(self, *humans: data_base.Human) -> None:
+		container = self.ids.content
+
+		[container.add_widget(FDHumanListItem(model=human)) \
+			for human in humans]
--- ui/frames/list_items.py
+++ ui/frames/list_items.py
@@ -1,8 +1,10 @@
+from typing import Union
+
 from kivy.lang.builder import Builder
 from kivy.properties import StringProperty
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelTwoLine
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton
+from kivymd.uix.button import MDIconButton, MDTextButton
 
 from app.path_manager import PathManager
 from config import paths
@@ -64,11 +66,30 @@ class FDEmergencyListItem(MDExpansionPanel):
 		self.model = model
 		self._emergency_content = EmergencyContent(self.model)
 
+		secondary_text = self.model.description
+		if secondary_text is None:
+			secondary_text = '-'
+
 		super().__init__(
 			icon=self.model.icon,
 			content=self._emergency_content,
 			panel_cls=MDExpansionPanelTwoLine(
 				text=self.model.title,
-				secondary_text=self.model.description
+				secondary_text=secondary_text
 			)
 		)
+
+
+class FDHumanListItem(MDBoxLayout):
+	''' Элемент списка контактов '''
+
+	def __init__(self, model: data_base.Human):
+		self.model = model
+
+		super().__init__()
+
+		self.ids.icon_btn.bind(on_release=self._show_info)
+		self.ids.text_btn.bind(on_release=self._show_info)
+
+	def _show_info(self, instance: Union[MDIconButton, MDTextButton]) -> None:
+		print(f'You pressed on {self.model.title} button')
