25dd62c62c51215dde95dd80b7528d79e9aa195c
USB-am <smit.acer@mail.ru>
2023-04-01 18:51:57 +0300

    Updated Current Calls screen UI

--- screens/current_calls.py
+++ screens/current_calls.py
@@ -2,6 +2,8 @@ from . import BaseScreen
 from app.path_manager import PathManager
 from data_base import Emergency
 
+from kivymd.uix.boxlayout import MDBoxLayout
+
 
 class CurrentCallsScreen(BaseScreen):
 	''' Экран текущих вызовов '''
@@ -15,3 +17,4 @@ class CurrentCallsScreen(BaseScreen):
 
 	def add_tab(self, entry: Emergency):
 		print(f'Add tag {entry.title}')
+		self.add_widgets(MDBoxLayout())
