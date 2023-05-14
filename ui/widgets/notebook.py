fbe081d0f8b720ea40b54825a5a181ffee3289fe
Usbam <smit.acer@mail.ru>
2023-05-14 15:52:50 +0300

    Created move to FDNotebook tab

--- ui/widgets/notebook.py
+++ ui/widgets/notebook.py
@@ -1,5 +1,4 @@
 from kivy.lang.builder import Builder
-from kivy.properties import StringProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 
 from config.paths import NOTEBOOK_WIDGET
@@ -41,10 +40,26 @@ class FDNotebook(MDBoxLayout):
 	def add_tab(self, entry: Emergency) -> None:
 		''' Добавляет новую вкладку '''
 
+		new_tab = FDTab(self, entry)
+		self.tabs.append(new_tab)
+		self.ids.top_bar.add_widget(new_tab.top_bar_tab)
+
+		self.move_to_tab(new_tab)
+
+	def move_to_tab(self, tab) -> None:
+		''' Собитие перехода на вкладку '''
 		self.clear_content()
 
-		new_tab = FDTab(self, entry)
-		self.ids.top_bar.add_widget(new_tab.top_bar_tab)
+		self.update_tabs_top_bg(tab)
+
+	def update_tabs_top_bg(self, active_tab) -> None:
+		''' Обновление заднего фона вкладки '''
+
+		for tab in self.tabs:
+			if active_tab is tab:
+				active_tab.top_bar_tab.active = True
+				break
+			tab.top_bar_tab.active = False
 
 	def clear_content(self) -> None:
 		''' Очищает содержимое вкладки '''
@@ -67,17 +82,27 @@ class FDTab:
 		self.entry = entry
 
 		self.top_bar_tab = FDTopBarTab(title=entry.title)
-		self.fill_content()
+		self.top_bar_tab.move_bind(callback=self.fill_content)
+		# self.fill_content()
 
 	def fill_content(self) -> None:
 		''' Заполняет содержимое контентом '''
+		self.parent.clear_content()
 
-		self.parent.ids.description_content.text = self.entry.description
+		description = self.entry.description
+		if description is None:
+			description = ''
+
+		self.parent.ids.description_content.text = description
 
 		for firefigher in self.entry.humans:
+			phone_1 = firefigher.phone_1
+			if phone_1 is None:
+				phone_1 = ''
+
 			triple_checkbox = FDTripleCheckbox(
 				title=firefigher.title,
-				substring=firefigher.phone_1
+				substring=phone_1
 			)
 			self.parent.ids.contacts_container.add_widget(triple_checkbox)
 
@@ -85,7 +110,28 @@ class FDTab:
 class FDTopBarTab(MDBoxLayout):
 	''' Ячейка в верхней прокручиваемой панели '''
 
-	title = StringProperty()
+	def __init__(self, title: str):
+		self.title = title
+		self._active = False
+		self._callback = lambda: print('Not callback')
+
+		super().__init__()
+
+	@property
+	def active(self) -> bool:
+		return self._active
+
+	@active.setter
+	def active(self, value: bool) -> None:
+		if value:
+			self.md_bg_color = (0, 0, 0, .05)
+		else:
+			self.md_bg_color = (0, 0, 0, 0)
+
+		self._active = value
+
+	def move_bind(self, callback) -> None:
+		self._callback = callback
 
 
 class FDTabContent(MDBoxLayout):
