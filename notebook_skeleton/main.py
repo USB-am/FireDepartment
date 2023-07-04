379a4951903fc4879214ca551aa9c3be464011de
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-07-04 12:31:28 +0300

    Added highlighting of the active tab

--- notebook_skeleton/main.py
+++ notebook_skeleton/main.py
@@ -8,7 +8,7 @@ from typing import Union, List, Callable
 
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, BooleanProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.boxlayout import MDBoxLayout
 
@@ -42,9 +42,26 @@ class TopPanelTab(MDBoxLayout):
 	''' Вкладка '''
 
 	title = StringProperty()
+	_state = BooleanProperty(False)
 
 	def bind_open_event(self, callback) -> None:
 		self.ids.open_btn.bind(on_release=callback)
+
+	@property
+	def state(self) -> bool:
+		return self._state
+
+	@state.setter
+	def state(self, value: bool) -> None:
+		self._state = value
+
+		self.activate() if value else self.deactivate()
+
+	def activate(self) -> None:
+		self.md_bg_color = [.5, .5, .5, .3]
+
+	def deactivate(self) -> None:
+		self.md_bg_color = [0, 0, 0, 0]
 
 
 class TopPanel(MDBoxLayout):
@@ -74,13 +91,14 @@ class TabContent:
 			for firefigher in self.firefighers
 		]
 
-	def get_data(self) -> dict:
+	def get_data(self, **kwargs) -> dict:
 		data = {
 			'title': self.db_entry.title,
 			'description': self.db_entry.description,
 			'subcontent': self.triple_checkboxes,
-			'tags': self.db_entry.tags,
+			'tags': self.db_entry.tags
 		}
+		data.update(kwargs)
 
 		return data
 
@@ -102,7 +120,7 @@ class Page:
 		self.content = TabContent(db_entry=emergency)
 
 		self.top_tab.bind_open_event(
-			lambda e: self.update_content_callable(self.content.get_data())
+			lambda e: self.update_content_callable(self.content.get_data(id=self.id))
 		)
 
 
@@ -119,18 +137,22 @@ class FDNotebook(MDBoxLayout):
 		self.ids.top_panel.add_tab(new_page.top_tab)
 
 		self.pages.append(new_page)
-		self.update_content(new_page.content.get_data())
+		self.update_content(new_page.content.get_data(id=new_page.id))
 
 	def update_content(self, data: dict) -> None:
-		print(f'FDNotebook.update_content is started')
 		self.ids.content_title.text = data['title']
 		self.ids.content_description.text = data['description']
 		self.ids.addition_information.text = data['title']
 
 		self.ids.subcontent.clear_widgets()
-
 		for element in data['subcontent']:
 			self.ids.subcontent.add_widget(element)
+
+		self._activate_tab(data['id'])
+
+	def _activate_tab(self, page_id: int) -> None:
+		for page in self.pages:
+			page.top_tab.state = page_id == page.id
 
 
 class TestScreen(Screen):
