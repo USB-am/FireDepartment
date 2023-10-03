99c402e4a49028bfcfbf4c238e9d9936d6282dda
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-10-03 17:33:46 +0300

    Created FDInput fields

--- config/paths.py
+++ config/paths.py
@@ -9,6 +9,11 @@ __KV_DIR = pjoin(__BASE_DIR, 'kv')
 __SCREENS_DIR = pjoin(__KV_DIR, 'screens')
 # __BASE_SCREEN_DIR = pjoin(__KV_DIR, 'base_screen.kv')
 
+# Fields
+__FIELDS_DIR = pjoin(__KV_DIR, 'fields')
+__FIELD_INPUT_DIR = pjoin(__FIELDS_DIR, 'input.kv')
+__FIELD_SELECT = pjoin(__FIELDS_DIR, 'select.kv')
+
 # Widgets
 __WIDGETS_DIR = pjoin(__KV_DIR, 'widgets')
 __TOOLBAR_DIR = pjoin(__WIDGETS_DIR, 'toolbar.kv')
