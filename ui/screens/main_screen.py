3a0fb51353df1efb95e031fbbca041ae292049a8
USB-am <smit.acer@mail.ru>
2023-10-12 22:33:56 +0300

    Filled list elements from data_base.Emergency model

--- ui/screens/main_screen.py
+++ ui/screens/main_screen.py
@@ -2,8 +2,6 @@ from app.path_manager import PathManager
 from data_base import Emergency
 from ui.screens.base_screen import BaseScreen
 from ui.widgets.list_element import MainScreenElement
-from ui.fields.switch import FDIconSwitch
-from kivymd.uix.button import MDFlatButton
 
 
 class MainScreen(BaseScreen):
@@ -16,17 +14,8 @@ class MainScreen(BaseScreen):
 		super().__init__(path_manager)
 
 		self.ids.toolbar.add_left_button(icon='menu', callback=self.open_menu)
+		self.ids.toolbar.add_right_button(icon='fire-truck', callback=lambda e: path_manager.forward('calls'))
 
 	def display(self) -> None:
-		w1 = FDIconSwitch(
-			icon='theme-light-dark',
-			title='Theme',
-			active_icon='white-balance-sunny',
-			deactive_icon='weather-night'
-		)
-		self.add_content(w1)
-		w1.set_value(True)
-
-		b = MDFlatButton(text='Test')
-		b.bind(on_release=lambda *e: print(w1.get_value()))
-		self.add_content(b)
+		for emergency in Emergency.query.all():
+			self.add_content(MainScreenElement(emergency))
