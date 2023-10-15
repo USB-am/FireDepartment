536f1da46eed59c9678c155b26f4dfaee751f839
USB-am <smit.acer@mail.ru>
2023-10-15 23:58:18 +0300

    Created frame for options screen

--- ui/screens/calls.py
+++ ui/screens/calls.py
@@ -1,6 +1,11 @@
+from kivy.lang.builder import Builder
+
 from app.path_manager import PathManager
 from data_base import Emergency
 from ui.screens.base_screen import BaseScreen
+from config.paths import __SCREEN_OPTIONS
+
+Builder.load_file(__SCREEN_OPTIONS)
 
 
 class CallsScreen(BaseScreen):
@@ -13,3 +18,6 @@ class CallsScreen(BaseScreen):
 		super().__init__(path_manager)
 
 		self.ids.toolbar.add_left_button(icon='arrow-left', callback=lambda e: path_manager.back())
+
+	def display(self) -> None:
+		print('All good!')
