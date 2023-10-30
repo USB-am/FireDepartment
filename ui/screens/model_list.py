326cdc8985e6b30f8136f14ccea4d552a3a7a2cc
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-10-30 16:41:59 +0300

    Created emergency_edit screen and connected with emergency_list screen

--- ui/screens/model_list.py
+++ ui/screens/model_list.py
@@ -5,7 +5,7 @@ from kivy.properties import ObjectProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 
 from app.path_manager import PathManager
-from data_base import db
+from data_base import db, Emergency
 from .base_screen import BaseScreen
 from config.paths import __SCREEN_MODEL_LIST
 
@@ -22,17 +22,21 @@ class ListElement(MDBoxLayout):
 class ModelList(BaseScreen):
 	""" Базовый класс отображения списка записей из БД """
 
-	def __init__(self, *, name: str, toolbar_title: str,\
-		model: db.Model, path_manager: PathManager):
-
-		self.name = name
-		self.model = model
-		self.toolbar_title = toolbar_title
-
+	def __init__(self, path_manager: PathManager):
 		super().__init__(path_manager)
 
 		self.ids.toolbar.add_left_button(icon='menu', callback=self.open_menu)
 
-		for entry in model.query.all():
+		for entry in self.model.query.all():
 			widget = ListElement(entry=entry)
+			widget.ids.button.bind(on_release=lambda *_: self.path_manager.forward(self.edit_link))
 			self.add_content(widget)
+
+
+class EmergencyList(ModelList):
+	''' Список вызовов '''
+
+	name = 'emergency_list'
+	toolbar_title = 'Список выездов'
+	model = Emergency
+	edit_link = 'emergency_edit'
