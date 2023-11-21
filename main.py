6cba38205b730c6fa5fefae2aa3f171169aaac27
USB-am <smit.acer@mail.ru>
2023-11-21 23:42:08 +0300

    Restart project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Delete this for prod
 from kivy.config import Config
 Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '650')
@@ -10,8 +9,12 @@ from data_base import db
 from app import Application
 
 
-if __name__ == '__main__':
+def main() -> None:
 	db.create_all()
 
 	app = Application()
 	app.run()
+
+
+if __name__ == '__main__':
+	main()
--- ui/widgets/list_element/__init__.py
+++
@@ -1,6 +0,0 @@
-from .main_screen_element import MainScreenElement
-
-
-__all__ = (
-	'MainScreenElement',
-)
