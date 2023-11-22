077215f8ff327ef9487003c18ecfea5e90705ab0
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-11-22 16:17:05 +0300

    Created menu and FDToolbar + connected them on MainScreen

---
+++ ui/widgets/toolbar.py
@@ -0,0 +1,13 @@
+from typing import Callable
+
+from kivymd.uix.toolbar import MDToolbar
+
+
+class FDToolbar(MDToolbar):
+	''' Верхняя полоска '''
+
+	def add_left_button(self, icon: str, callback: Callable) -> None:
+		self.left_action_items.append([icon, callback])
+
+	def add_right_button(self, icon: str, callback: Callable) -> None:
+		self.right_action_items.append([icon, callback])
