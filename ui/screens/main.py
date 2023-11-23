f0ecf424552c5fe45ebe3ebaf787d7b4e5b4fa16
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-11-23 14:42:48 +0300

    Created PathManager and connects with screens

--- ui/screens/main.py
+++ ui/screens/main.py
@@ -2,6 +2,8 @@ from . import BaseScreen, BaseScrollScreen
 from kivymd.uix.label import MDLabel
 from kivy.metrics import dp
 
+from app.path_manager import PathManager
+
 
 class MainScreen(BaseScrollScreen):
 	''' Стартовая страница '''
@@ -9,13 +11,18 @@ class MainScreen(BaseScrollScreen):
 	name = 'main'
 	toolbar_title = 'Главная'
 
-	def __init__(self, **options):
-		super().__init__()
+	def __init__(self, path_manager: PathManager, **options):
+		super().__init__(path_manager)
 
 		self.ids.toolbar.add_left_button(
 			icon='menu',
 			callback=self.open_menu
 		)
+		self.ids.toolbar.add_right_button(
+			icon='fire-truck',
+			callback=lambda *_: self._path_manager.forward('calls')
+		)
+
 		for i in range(20):
 			self.add_content(MDLabel(
 				text=f'Row #{i+1}',
