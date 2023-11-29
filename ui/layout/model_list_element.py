3c42a621631d7b18b1f357fb0c0d5c3e767b8d50
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-11-29 17:35:22 +0300

    Finished ModelListScreen and layouts for this screen

---
+++ ui/layout/model_list_element.py
@@ -0,0 +1,47 @@
+from typing import Callable
+
+from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
+
+from data_base import db
+from config import MODEL_LIST_LAYOUTS
+
+
+Builder.load_file(MODEL_LIST_LAYOUTS)
+
+
+class _ModelListElementContent(MDBoxLayout):
+	'''
+	Содержимое выпадающей области элемента.
+
+	~params:
+	entry: db.Model - запись из БД.
+	'''
+
+	entry = ObjectProperty(defaultvalue=db.Model)
+
+
+class ModelListElement(MDExpansionPanel):
+	'''
+	Элемента списка модели.
+
+	entry: db.Model - запись из БД.
+	'''
+
+	def __init__(self, entry: db.Model, **options):
+		self.entry = entry
+
+		options.update({
+			'content': _ModelListElementContent(entry=entry),
+			'panel_cls': MDExpansionPanelOneLine(text=entry.title)
+		})
+
+		super().__init__(**options)
+
+	def bind_edit_btn(self, callback: Callable) -> None:
+		self.content.ids.edit_btn.bind(on_release=lambda *_: callback())
+
+	def bind_info_btn(self, callback: Callable) -> None:
+		self.content.ids.info_btn.bind(on_release=lambda *_: callback())
--- ui/screens/model_list.py
+++ ui/screens/model_list.py
@@ -3,6 +3,7 @@ from typing import Type
 from . import BaseScrollScreen
 from app.path_manager import PathManager
 from data_base import db, Tag
+from ui.layout.model_list_element import ModelListElement
 
 
 __all__ = ('TagsList',)
@@ -19,19 +20,26 @@ class _ModelList(BaseScrollScreen):
 			callback=self.open_menu
 		)
 		self.ids.toolbar.add_right_button(
-			icon='note-plus',
+			icon='file-plus',
 			callback=None
 		)
 
-		for model in self.model.query.all():
-			print(model.title)
+		self.fill_elements()
+
+	def fill_elements(self) -> None:
+		pass
 
 
 class TagsList(_ModelList):
-	'''
-	Класс с элементами из модели Tag.
-	'''
+	''' Класс с элементами из модели Tag '''
 
 	name = 'tags_list'
 	model = Tag
 	toolbar_title = 'Теги'
+
+	def fill_elements(self) -> None:
+		for tag in self.model.query.all():
+			list_elem = ModelListElement(entry=tag, icon=Tag.icon)
+			list_elem.bind_edit_btn(lambda t=tag: print(f'Edit btn {t.title}'))
+			list_elem.bind_info_btn(lambda t=tag: print(f'Info btn {t.title}'))
+			self.add_content(list_elem)
