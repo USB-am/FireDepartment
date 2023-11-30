493bc66816ae327a2633b84d0d73c7b9f106e27d
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-11-30 16:04:23 +0300

    Created and included <Model>_list screens

--- ui/screens/model_list.py
+++ ui/screens/model_list.py
@@ -2,7 +2,7 @@ from typing import Type
 
 from . import BaseScrollScreen
 from app.path_manager import PathManager
-from data_base import db, Tag
+from data_base import db, Tag, Rank, Position, Human, Emergency
 from ui.layout.model_list_element import ModelListElement
 
 
@@ -42,4 +42,68 @@ class TagsList(_ModelList):
 			list_elem = ModelListElement(entry=tag, icon=Tag.icon)
 			list_elem.bind_edit_btn(lambda t=tag: print(f'Edit btn {t.title}'))
 			list_elem.bind_info_btn(lambda t=tag: print(f'Info btn {t.title}'))
+			self.add_content(list_elem)
+
+
+class RanksList(_ModelList):
+	''' Класс с элементами из модели Rank '''
+
+	name = 'ranks_list'
+	model = Rank
+	toolbar_title = 'Звания'
+
+	def fill_elements(self) -> None:
+		for rank in self.model.query.all():
+			list_elem = ModelListElement(entry=rank, icon=Rank.icon)
+			list_elem.bind_edit_btn(lambda r=rank: print(f'Edit btn {r.title}'))
+			list_elem.bind_info_btn(lambda r=rank: print(f'Info btn {r.title}'))
+			self.add_content(list_elem)
+
+
+class PositionsList(_ModelList):
+	''' Класс с элементами из модели Position '''
+
+	name = 'positions_list'
+	model = Position
+	toolbar_title = 'Должности'
+
+	def fill_elements(self) -> None:
+		for position in self.model.query.all():
+			list_elem = ModelListElement(entry=position, icon=Position.icon)
+			list_elem.bind_edit_btn(lambda p=position: print(f'Edit btn {p.title}'))
+			list_elem.bind_info_btn(lambda p=position: print(f'Info btn {p.title}'))
+			self.add_content(list_elem)
+
+
+class HumansList(_ModelList):
+	''' Класс с элементами из модели Human '''
+
+	name = 'humans_list'
+	model = Human
+	toolbar_title = 'Сотрудники'
+
+	def fill_elements(self) -> None:
+		for human in self.model.query.all():
+			i = 'fire-hydrant' if human.is_firefigher else Human.icon
+			list_elem = ModelListElement(
+				entry=human,
+				icon=i
+			)
+			list_elem.bind_edit_btn(lambda h=human: print(f'Edit btn {h.title}'))
+			list_elem.bind_info_btn(lambda h=human: print(f'Info btn {h.title}'))
+			self.add_content(list_elem)
+
+
+class EmergenciesList(_ModelList):
+	''' Класс с элементами из модели Emergency '''
+
+	name = 'emergencies_list'
+	model = Emergency
+	toolbar_title = 'Вызовы'
+
+	def fill_elements(self) -> None:
+		for emergency in self.model.query.all():
+			list_elem = ModelListElement(entry=emergency, icon=Emergency.icon)
+			list_elem.bind_edit_btn(lambda e=emergency: print(f'Edit btn {e.title}'))
+			list_elem.bind_info_btn(lambda e=emergency: print(f'Info btn {e.title}'))
 			self.add_content(list_elem)
