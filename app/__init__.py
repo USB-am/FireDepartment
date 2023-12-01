bff96e794442728fa57d55684e38e65d834674ac
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-12-01 16:29:04 +0300

    Created BaseDialogContent skeleton

--- app/__init__.py
+++ app/__init__.py
@@ -76,7 +76,7 @@ class Application(MDApp):
 		self.ui.screen_manager.add_widget(HumansList(self.ui.path_manager))
 		self.ui.screen_manager.add_widget(EmergenciesList(self.ui.path_manager))
 
-		self.ui.path_manager.move_to_screen('main')
+		self.ui.path_manager.move_to_screen('tags_list')
 
 	def build(self):
 		return self.ui
