123bd154171c57fcdea4bb2d209d941af2655e6b
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-12-19 11:31:08 +0300

    Added method for deleting icon-buttons

--- ui/widgets/toolbar.py
+++ ui/widgets/toolbar.py
@@ -18,3 +18,9 @@ class FDToolbar(MDToolbar):
 
 	def add_right_button(self, icon: str, callback: Callable) -> None:
 		self.right_action_items.append([icon, callback])
+
+	def rem_left_button(self) -> None:
+		self.left_action_items.pop()
+
+	def rem_right_button(self) -> None:
+		self.right_action_items.pop()
