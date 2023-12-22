ff15d1c5849b48175627a3e3a71898d0850a7df7
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-12-22 16:16:54 +0300

    Created FDButtonDropdown field and included to Options screen

--- ui/screens/options.py
+++ ui/screens/options.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from kivymd.app import MDApp
 from kivymd.theming import ThemeManager
 from kivymd.uix.picker import MDThemePicker
 
 from . import BaseScrollScreen
 from app.path_manager import PathManager
-from ui.field.button import FDButton
+from ui.field.button import FDButton, FDButtonDropdown
 from config import CONF
 
 
@@ -13,27 +15,58 @@ class OptionsScreen(BaseScrollScreen):
 
 	name = 'options'
 	toolbar_title = 'Настройки'
-	config = None	# Обновляется в методе build класса App
 
 	def __init__(self, path_manager: PathManager):
 		super().__init__(path_manager)
+
+		self.__app = MDApp.get_running_app()
+		# self.__theme_cls = self.__app.theme_cls
 
 		self.ids.toolbar.add_left_button(
 			icon='menu',
 			callback=self.open_menu
 		)
 
-		self.fill_elements()
+		self.bind(on_pre_enter=lambda *_: self.fill_elements())
+
+	@property
+	def __theme_cls(self):
+		return self.__app.theme_cls
+
+	@property
+	def __config(self):
+		return self.__app.config
 
 	def fill_elements(self) -> None:
-		self.color_theme = FDButton(
+		self.clear_content()
+
+		self.color_theme_field = FDButton(
 			icon='palette',
 			title='Цветовая схема',
 			btn_text='Выбрать'
 		)
-		self.color_theme.ids.btn.bind(on_release=lambda *_: self._open_theme_picker())
+		self.color_theme_field.ids.btn.bind(on_release=lambda *_: self._open_theme_picker())
 
-		self.add_content(self.color_theme)
+		self.hue_field = FDButtonDropdown(
+			icon='opacity',
+			title='Контрастность',
+		)
+		self.hue_field.update_elements(self._gen_hue_field_elements())
+		self.hue_field.elems.append({'text': 'test', 'viewclass': 'OneLineListItem'})
+
+		self.add_content(self.color_theme_field)
+		self.add_content(self.hue_field)
+
+	def _gen_hue_field_elements(self) -> List:
+		''' Возвращает элементы для поля hue_field '''
+
+		return [{
+				'text': elem,
+				'viewclass': 'OneLineListItem',
+				'bg_color': self.__theme_cls.colors[self.__theme_cls.primary_palette][elem],
+				'on_release': lambda elem=elem: self._update_hue_field(elem),
+			} for elem in self.__theme_cls.colors[self.__theme_cls.primary_palette].keys()
+		]
 
 	def _open_theme_picker(self) -> None:
 		''' Открыть диалоговое окно выбора темы '''
@@ -42,16 +75,20 @@ class OptionsScreen(BaseScrollScreen):
 		dialog.open()
 
 	def __save_colors_to_config(self) -> None:
-		if self.config is None:
-			return
-
-		app = MDApp.get_running_app()
-		config = app.config
-		theme_cls = app.theme_cls
+		config = self.__config
 
 		config.setall('options', {
-			'primary_palette': theme_cls.primary_palette,
-			'accent_palette': theme_cls.accent_palette,
-			'theme_style': theme_cls.theme_style,
+			'primary_palette': self.__theme_cls.primary_palette,
+			'accent_palette': self.__theme_cls.accent_palette,
+			'theme_style': self.__theme_cls.theme_style,
+			'primary_hue': self.__theme_cls.primary_hue,
 		})
 		config.write()
+
+	def _update_hue_field(self, element) -> None:
+		self.hue_field.ids.btn.text = element
+		self.hue_field.update_elements(self._gen_hue_field_elements())
+		self.hue_field.dropdown.dismiss()
+
+		self.__theme_cls.primary_hue = element
+		self.__save_colors_to_config()
