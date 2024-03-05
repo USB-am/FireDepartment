3fe1b9e33c9ea8e06eaa7d3c8a61d9e6e073f7b4
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-03-05 17:34:23 +0300

    Extended paginator env

--- paginator.py
+++ paginator.py
@@ -1,3 +1,6 @@
+from time import time
+from dataclasses import dataclass
+
 from kivymd.app import MDApp
 from kivy.metrics import dp
 from kivy.lang.builder import Builder
@@ -5,6 +8,8 @@ from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import *
 from kivymd.uix.label import MDLabel
 from kivymd.uix.textfield import MDTextField
+
+from data_base import Emergency
 
 
 Builder.load_string('''
@@ -34,16 +39,29 @@ Builder.load_string('''
 ''')
 
 
+def get_elements(widget, params):
+	return tuple(
+		widget(text=f'Label #{i+1}', size_hint=(1, None), height=dp(50)) \
+		for i in params
+	)
+
+
 class Search(MDTextField):
+	def __init__(self, **options):
+		super().__init__(**options)
+		self.callbacks = []
+
 	def on_text_validate(self):
 		output = super().on_text_validate()
-		print('pressed on search')
+
+		[callback(self.text) for callback in self.callbacks]
+
 		return output
 
 
 class Paginator(list):
 	def __init__(self, *elements):
-		super().__init__(*elements)
+		super().__init__(elements)
 
 	def paginate_by(self, count: int):
 		while self:
@@ -51,32 +69,70 @@ class Paginator(list):
 			self = self[count:]
 
 
+@dataclass
+class ElementController:
+	def __init__(self, model, paginator, search):
+		self.model = model
+		self.paginator = paginator
+		self.search = search
+
+		# Empty generator
+		self.current_paginator = iter(())
+
+	def get_paginator(self, count=10):
+		self.current_paginator = self.paginator.paginate_by(count)
+
+		return self.current_paginator
+
+	def find_elements(self, text):
+		filtered_entries = self.model.query\
+			.filter(self.model.title.like(f'%{text}%'))\
+			.order_by(self.model.title)\
+			.all()
+		filtered_entries = [i for i in range(100)]
+		self.paginator = Paginator(*get_elements(MDLabel, filtered_entries))
+
+		return self.get_paginator()
+
+
 class MainScreen(Screen):
-	def __init__(self):
-		super().__init__()
+	model = Emergency
 
-		self.paginator = Paginator()
+	def __init__(self, **options):
+		super().__init__(**options)
 
-		for i in range(100):
-			label = MDLabel(
-				text=f'Label #{i+1}',
-				size_hint=(1, None),
-				height=dp(50)
-			)
-			# self.content.add_widget(label)
-			self.paginator.append(label)
+		self.paginator = Paginator(*get_elements(MDLabel, [i for i in range(100)]))
+		self.last_call = 0
 
-		self.gen = self.paginator.paginate_by(10)
-		[self.content.add_widget(el) for el in next(self.gen)]
+		self.element_controller = ElementController(
+			model=self.model,
+			paginator=self.paginator,
+			search=self.ids.search
+		)
+		self.ids.search.callbacks.append(
+			lambda text: self.search_elements(text)
+		)
+
+		self.elements = self.element_controller.get_paginator()
+		[self.content.add_widget(el) for el in next(self.elements)]
+
+	def search_elements(self, text):
+		self.ids.content.clear_widgets()
+		gen = self.element_controller.find_elements(text)
+		[self.content.add_widget(el) for el in next(gen)]
 
 	def end_list_event(self):
+		if self.last_call > time() - 1:
+			return
+
 		try:
-			[self.content.add_widget(el) for el in next(self.gen)]
-			# elements_count = len(self.content.children)
-			# to_scroll_percent = 10 / elements_count / 10
-			# self.ids.scrollbar.scroll_y = to_scroll_percent
+			elements = self.element_controller.current_paginator
+			[self.content.add_widget(el) for el in next(elements)]
+			# self.ids.scrollbar.scroll_to(self.content.children[10])
 		except StopIteration:
 			pass
+
+		self.last_call = time()
 
 	@property
 	def content(self):
@@ -88,7 +144,6 @@ class MyApp(MDApp):
 		super().__init__(*a, **kw)
 
 		self.screen = MainScreen()
-		# self.screen.add_widget(TestBtn())
 
 	def build(self):
 		return self.screen
