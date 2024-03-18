e7c2e19176ec076e9afd8fd2f9b7ea8e1e8f307a
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-03-18 15:34:44 +0300

    Created logic for updating paginator count elements

--- ui/screens/options.py
+++ ui/screens/options.py
@@ -6,6 +6,8 @@ from . import BaseScrollScreen
 from app.path_manager import PathManager
 from ui.field.button import FDButtonDropdown
 from ui.field.switch import FDDoubleSwitch
+from ui.field.input import FDNumberInput
+from config import ITERABLE_COUNT
 
 
 class OptionsScreen(BaseScrollScreen):
@@ -23,6 +25,10 @@ class OptionsScreen(BaseScrollScreen):
 			icon='menu',
 			callback=self.open_menu
 		)
+		self.ids.toolbar.add_right_button(
+			icon='check',
+			callback=lambda *_: self.save_changes()
+		)
 
 		self.bind(on_pre_enter=lambda *_: self.fill_elements())
 
@@ -34,15 +40,29 @@ class OptionsScreen(BaseScrollScreen):
 	def __config(self):
 		return self.__app.config
 
+	def save_changes(self) -> None:
+		''' Сохранить настройки в application.ini '''
+
+		self.__save_colors_to_config()
+		try:
+			ITERABLE_COUNT = int(self.iterable_count_field.text)
+		except ValueError:
+			pass
+
+		self._path_manager.back()
+
 	def fill_elements(self) -> None:
 		self.clear_content()
 
+		# Основной цвет
 		self.primary_field = FDButtonDropdown(
 			icon='palette',
 			title='Основной цвет',
 		)
 		self.primary_field.update_elements(self._gen_primary_field_elements())
 		self.primary_field.ids.btn.md_bg_color = self.__theme_cls.primary_color
+
+		# Акцентирующий цвет
 		self.accent_field = FDButtonDropdown(
 			icon='palette-advanced',
 			title='Акцентирующий цвет'
@@ -50,12 +70,14 @@ class OptionsScreen(BaseScrollScreen):
 		self.accent_field.update_elements(self._gen_accent_field_elements())
 		self.accent_field.ids.btn.md_bg_color = self.__theme_cls.accent_color
 
+		# Контрастность
 		self.hue_field = FDButtonDropdown(
 			icon='opacity',
 			title='Контрастность',
 		)
 		self.hue_field.update_elements(self._gen_hue_field_elements())
 
+		# Тема
 		self.theme_style_field = FDDoubleSwitch(
 			icon_active='moon-waning-crescent',
 			title_active='Темная тема',
@@ -66,10 +88,19 @@ class OptionsScreen(BaseScrollScreen):
 			self._update_style_field(self.theme_style_field.ids.switch.active)
 		)
 
+		# Количество подгружаемых элементов в пагинаторе
+		self.iterable_count_field = FDNumberInput(
+			hint_text='Элементы на странице',
+			helper_text='Увеличение значения может замедлить работу приложения!'
+		)
+		self.iterable_count_field.set_value(ITERABLE_COUNT)
+		self.iterable_count_field.bind(on_text_validate=lambda *_: print(_))
+
 		self.add_content(self.primary_field)
 		self.add_content(self.accent_field)
 		self.add_content(self.hue_field)
 		self.add_content(self.theme_style_field)
+		self.add_content(self.iterable_count_field)
 
 	def _gen_primary_field_elements(self) -> List:
 		''' Возвращает элементы для поля primary '''
@@ -121,6 +152,9 @@ class OptionsScreen(BaseScrollScreen):
 			'theme_style': self.__theme_cls.theme_style,
 			'primary_hue': self.__theme_cls.primary_hue,
 		})
+		config.setall('global', {
+				'iterable_count': self.iterable_count_field.text,
+			})
 		config.write()
 
 	def _update_primary_field(self, element: str) -> None:
