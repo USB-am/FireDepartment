3c22636a5f0773b3c4fb55f5702a3b283168bdf2
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-04-15 15:59:50 +0300

    Created skeleton for call manager

---
+++ call_manager/temp.py
@@ -0,0 +1,50 @@
+
+
+class FDTripleCheckbox:
+	def __init__(self, title, substring, state=0):
+		self.title = title
+		self.substring = substring
+		self.state = state
+
+	def __str__(self):
+		return f'[{self.state}] {self.title}'
+
+
+class PhoneManager:
+	def __init__(self, *phones):
+		self.phones = list(phones)
+
+	def add_phone(self, phone):
+		self.phones.appned(phone)
+
+
+class CallManager:
+	def __init__(self, phones):
+		self.phone_manager = PhoneManager(*phones)
+
+	def add_phone(self, phone):
+		self.phone_manager.add_phone(phone)
+
+
+class CallController:
+	def __init__(self, boxlayout):
+		self.boxlayout = boxlayout
+		self.call_manager = CallManager(boxlayout.phones)
+
+
+def get_all_checkboxes():
+	return [FDTripleCheckbox(
+		title=f'Phone #{i+1}',
+		substring=f'Substring #{i+1}',
+		state=i%3) for i in range(50)
+	]
+
+class BoxLayout_:
+	def __init__(self):
+		self.phones = get_all_checkboxes()
+
+
+if __name__ == '__main__':
+	boxlayout = BoxLayout_()
+	cc = CallController(boxlayout)
+	print(*cc.call_manager.phone_manager.phones, sep='\n')
