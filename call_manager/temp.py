0fff68c5d196f21a8085bb39d12ac21efa81e163
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-04-18 16:56:50 +0300

    Created ShortsManager for call manager

--- call_manager/temp.py
+++ call_manager/temp.py
@@ -31,6 +31,15 @@ class FDTripleCheckbox:
 			return f'Unselected {self.title}'
 
 
+class Short:
+	def __init__(self, title: str, explanation: str):
+		self.title = title
+		self.explanation = explanation
+
+	def __str__(self):
+		return self.title
+
+
 class LogElement:
 	def __init__(self, msg: str):
 		self.msg = msg
@@ -40,14 +49,15 @@ class LogElement:
 		return f'{self.timestamp} {self.msg}'
 
 
-class PhoneLogger(dict):
+class Logger(dict):
 	def add_log(self, obj: Any, msg: str) -> None:
-		log = LogElement(msg=msg)
+		log = LogElement(msg)
+		obj_id = id(obj)
 
-		if id(obj) in self:
-			self[id(obj)].append(log)
+		if obj_id in self:
+			self[obj_id].append(log)
 		else:
-			self[id(obj)] = [log,]
+			self[obj_id] = [log,]
 
 	def _all_logs(self) -> str:
 		output = ''
@@ -60,6 +70,7 @@ class PhoneLogger(dict):
 		return '\n'.join([f'{key} {logs[-1]}' for key, logs in self.items()])
 
 
+# === PHONE MANAGER === #
 class PhoneList(list):
 	def __init__(self, *args, callback=lambda e: None):
 		super().__init__(args)
@@ -77,10 +88,10 @@ class PhoneList(list):
 class PhoneManager(PhoneList):
 	def __init__(self, *phones):
 		super().__init__(*phones)
-		self.logger = PhoneLogger()
+		self.logger = Logger()
 
 	def add_phones(self, *phones):
-		self.phones.extend(phones)
+		self.extend(phones)
 
 	def select_phone(self, phone: FDTripleCheckbox) -> None:
 		if phone not in self:
@@ -88,26 +99,66 @@ class PhoneManager(PhoneList):
 
 		phone.state += 1
 		self.logger.add_log(phone, str(phone))
+# === PHONE MANAGER === #
+
+
+# === SHORTS MANAGER === #
+class ShortsList(list):
+	def __init__(self, *args, callback=lambda e: None):
+		super().__init__(args)
+		self.callback = callback
+
+	def append(self, elem) -> None:
+		super().append(elem)
+		self.callback(elem)
+
+	def extend(self, *elems) -> None:
+		super().extend(elems)
+		self.callback(*elems)
+
+
+class ShortsManager(ShortsList):
+	def __init__(self, *shorts):
+		super().__init__(*shorts)
+		self.logger = Logger()
+
+	def add_shorts(self, *shorts: Short) -> None:
+		self.extend(shorts)
+
+	def select_short(self, short) -> None:
+		if short not in self:
+			self.add_shorts(short)
+
+		self.logger.add_log(short, str(short))
+
+# === SHORTS MANAGER === #
 
 
 class CallManager:
 	def __init__(self, phones):
 		self.phone_manager = PhoneManager(*phones)
+		self.shorts_manager = ShortsManager(emergency.shorts)
 
 	def add_phone(self, phone):
 		self.phone_manager.add_phone(phone)
 
+	def add_short(self, short):
+		self.shorts_manager.add_shorts(short)
+
 	def __str__(self):
-		return str(self.phone_manager.logger)
+		return str(self.shorts_manager.logger)
 
 
 class CallController:
-	def __init__(self, boxlayout):
+	def __init__(self, emergency, boxlayout):
 		self.boxlayout = boxlayout
 		self.call_manager = CallManager(boxlayout.phones)
 
 	def select_phone(self, phone: FDTripleCheckbox) -> None:
 		self.call_manager.phone_manager.select_phone(phone)
+
+	def select_short(self, short: Short) -> None:
+		self.call_manager.shorts_manager.select_short(short)
 
 
 def get_all_checkboxes():
@@ -121,12 +172,35 @@ class BoxLayout_:
 	phones = get_all_checkboxes()
 
 
+def gen_shorts():
+	return [Short(
+	              title=f'Short #{2**i}',
+	              explanation=f'Explanation for Short #{2**i}')
+		for i in range(10)
+	]
+
+class Emergency:
+	def __init__(self, title, shorts: list):
+		self.title = title
+		self.shorts = shorts
+
+	def __str__(self):
+		return self.title
+
+
 if __name__ == '__main__':
 	boxlayout = BoxLayout_()
-	cc = CallController(boxlayout)
-	cc.select_phone(BoxLayout_.phones[0])
-	cc.select_phone(BoxLayout_.phones[0])
-	cc.select_phone(BoxLayout_.phones[0])
-	cc.select_phone(BoxLayout_.phones[1])
-	cc.select_phone(BoxLayout_.phones[2])
+	emergency = Emergency('Emergency #1', gen_shorts())
+	cc = CallController(emergency, boxlayout)
+	cc.select_short(emergency.shorts[0])
+	cc.select_short(emergency.shorts[0])
+	cc.select_short(emergency.shorts[1])
+	cc.select_short(emergency.shorts[2])
 	print(cc.call_manager)
+
+	# cc.select_phone(BoxLayout_.phones[0])
+	# cc.select_phone(BoxLayout_.phones[0])
+	# cc.select_phone(BoxLayout_.phones[0])
+	# cc.select_phone(BoxLayout_.phones[1])
+	# cc.select_phone(BoxLayout_.phones[2])
+	# print(cc.call_manager)
