8693e7231eb3dfc10f701589782dfb232d77bb77
USB-am <smit.acer@mail.ru>
2024-10-21 14:03:18 +0300

    Added move_to_tab and close_tab methods for FDNotebook widget

--- notebook_template/widgets/notebook.py
+++ notebook_template/widgets/notebook.py
@@ -1,8 +1,9 @@
 from typing import List
 from collections.abc import Callable
 
-from kivy.lang.builder import Builder
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.lang.builder import Builder # type: ignore
+from kivymd.uix.boxlayout import MDBoxLayout # type: ignore
+from kivymd.uix.label import MDLabel
 
 from config import NOTEBOOK_WIDGET_KV
 
@@ -51,17 +52,49 @@ class FDNotebook(MDBoxLayout):
 
 	def __init__(self, **options):
 		super().__init__(**options)
+		self.current_tab = None
 		self.__tabs: List[FDTab] = []
 
-		for i in range(30):
+		for i in range(5):
 			txt = f'Tab #{i+1}'
 			content = MDBoxLayout()
+			content.add_widget(MDLabel(text=txt))
 			tab = FDTab(title=txt, content=content)
 			self.add_tab(tab)
 
 	def add_tab(self, tab: FDTab) -> None:
 		''' Добавить вкладку '''
 
-		tab.bind_click(lambda inst: print(inst.parent.title))
-		tab.bind_close(lambda inst: print(f'Click by close {inst}'))
+		self.__tabs.append(tab)
+
+		tab.bind_click(lambda inst, t=tab: self.move_to_tab(t))
+		tab.bind_close(lambda inst, t=tab: self.close_tab(t))
 		self.ids.tab_list.add_widget(tab._marker)
+
+		self.move_to_tab(tab)
+
+	def move_to_tab(self, tab: FDTab) -> None:
+		''' Переключиться на вкладку '''
+
+		content_layout = self.ids.content
+		for content in content_layout.children:
+			content_layout.remove_widget(content)
+
+		content_layout.add_widget(tab.content)
+		self.current_tab = tab
+
+	def close_tab(self, tab: FDTab) -> None:
+		''' Закрыть вкладку '''
+
+		self.ids.tab_list.remove_widget(tab._marker)
+		self.ids.content.remove_widget(tab.content)
+
+		self.__tabs.remove(tab)
+
+		if tab is self.current_tab:
+			try:
+				self.move_to_tab(self.__tabs[0])
+			except IndexError:
+				self.ids.content.add_widget(MDLabel(
+					text='Ничего не выбрано', halign='center',
+					valign='center'))
