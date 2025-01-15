ea1eb8e71599b14943cc0402680223c4a1151eb1
USB-am <smit.acer@mail.ru>
2025-01-15 06:57:58 +0300

    Updated tests for FDCalendar widget

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -17,20 +17,9 @@ class TestFDCalendar(unittest.TestCase):
 	def setUpClass(cls):
 		cls.NOW_DATE = datetime(2024, 12, 10)
 		cls.app = AppTest()
-		cls.date_field = FDDate(
-			icon='account',
-			title='Test FDDate',
-			btn_text='test btn')
-		cls.date_field.set_value(cls.NOW_DATE)
-		cls.worktype_field = FDSelect(
-			title='Test FDSelect',
-			dialog_content=MDBoxLayout(),
-			model=Worktype,
-			group='test_worktype_group')
-		cls.worktype_field.set_value(Worktype.query.first())
-		cls.calendar = Calendar.FDCalendar(
-			from_date_field=cls.date_field,
-			worktype_field=cls.worktype_field)
+
+		cls.calendar = Calendar.FDCalendar()
+		cls.calendar._now_date = cls.NOW_DATE
 
 		cls.app.screen.add_widget(cls.calendar)
 
@@ -39,25 +28,25 @@ class TestFDCalendar(unittest.TestCase):
 		cls.app.screen.remove_widget(cls.calendar)
 
 	def setUp(self):
-		self.calendar.update(self.NOW_DATE)
+		self.calendar._now_date = self.NOW_DATE
 
 	def test_next_month(self):
 		now_month = self.calendar.now_date.month
-		self.calendar.next_month()
+		self.calendar.move_to_next_month()
 		next_month = self.calendar.now_date.month
 
 		self.assertEqual((now_month + 1) % 12, next_month)
 
 	def test_prev_month(self):
 		now_month = self.calendar.now_date.month
-		self.calendar.prev_month()
+		self.calendar.move_to_prev_month()
 		prev_month = self.calendar.now_date.month
 		current_month = now_month - 1 if now_month - 1 > 0 else 12
 
 		self.assertEqual(current_month, prev_month)
 
 	def test_add_month(self):
-		func_result = Calendar.add_months(self.NOW_DATE, 1)
+		func_result = Calendar.add_months(self.NOW_DATE.date(), 1)
 		expected = date(
 			year=self.NOW_DATE.year if self.NOW_DATE.month + 1 <= 12 else self.NOW_DATE.year + 1,
 			month=(self.NOW_DATE.month + 1) % 12,
