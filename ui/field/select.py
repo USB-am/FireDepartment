fe1331872e7f91c80b38db2bee7a8a8e26e4a4c6
USB-am <smit.acer@mail.ru>
2025-01-22 23:43:02 +0300

    Merged new recycle select widget with main project

--- ui/field/select.py
+++ ui/field/select.py
@@ -1,6 +1,12 @@
-from typing import Callable, List, Union, Type, Set
+from typing import Callable, List, Union, Type, Set, Dict
 
 from kivy.lang.builder import Builder
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
 
 from data_base import db
@@ -136,3 +142,91 @@ class FDMultiSelect(_BaseSelect):
 				element.ids.checkbox.active = True
 			else:
 				element.ids.checkbox.active = False
+
+
+class FDSelectElement(RecycleDataViewBehavior, MDBoxLayout):
+	''' Элемент списка FDSelect '''
+
+	index = None
+	text = StringProperty()
+	group = StringProperty(None)
+	active = BooleanProperty()
+
+	def refresh_view_attrs(self, rv, index, data):
+		self.index = index
+		return super().refresh_view_attrs(rv, index, data)
+
+	def store_checkbox_state(self):
+		rv = self.parent.parent
+		rv.update_checkbox_state(index=self.index,
+		                         state=self.active,
+		                         group=self.group)
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+	''' Предстваление области отображения элементов списка '''
+
+
+class _FDSelectRecycleView(RecycleView):
+	''' Вьюха списка элементов '''
+
+	def update_checkbox_state(self, index: int, state: bool, group: str) -> None:
+		''' Обновить состояния чекбоксов '''
+
+		if group:
+			for elem in self.data:
+				if elem['active']:
+					elem['active'] = False
+		self.data[index]['active'] = state
+
+
+class _BaseRecycleSelect(_BaseSelect):
+	''' Оптимизированный FDSelect '''
+
+	def __init__(self, data: List[Dict], **options):
+		super().__init__(**options)
+		self.ids.recycle_view.data = data
+
+		self.bind_btn(lambda *_: print(self.get_value()))
+
+	@property
+	def data(self) -> List[Dict]:
+		return self.ids.recycle_view.data
+
+	def fill_elements(self) -> None:
+		''' Заполняет список недостающими элементами из модели '''
+
+		# TODO
+
+
+class FDRecycleSelect(_BaseRecycleSelect):
+	''' Поле выбора одного элемента.
+
+	~params:
+	title: str - текст над списком;
+	dialog_content: MDBoxLayout - содержимое окна с информацией;
+	model: db.Model - модель БД, элементы которой будут отображены;
+	group: str - группа объектов. Если None, доступен выбор множества элементов.
+	'''
+
+	def get_value(self) -> int:
+		''' Получить id выбранного элемента '''
+
+		try:
+			return next(filter(lambda elem: elem['active'], self.data))
+		except StopIteration:
+			return None
+
+
+class FDRecycleMultiSelect(_BaseRecycleSelect):
+	''' Поле выбора множества элементов '''
+
+	def __init__(self, **kwargs):
+		kwargs.update({'group': None})
+		super().__init__(**kwargs)
+
+	def get_value(self) -> List:
+		''' Получить записи о выбранных элементах '''
+
+		filtered_elems = filter(lambda elem: elem['active'], self.data)
+		return list(map(lambda elem: elem['text'], filtered_elems))
