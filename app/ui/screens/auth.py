a80338393a3f43545164272dd80ab83a111919a8
USB-am <smit.acer@mail.ru>
2026-01-07 06:33:43 +0300

    Connected the auth and register screens with a new base screen

--- app/ui/screens/auth.py
+++ app/ui/screens/auth.py
@@ -1,12 +1,13 @@
 from kivymd.uix.boxlayout import MDBoxLayout
 
-from .base import BaseScreen
+from .base import BaseAuthScreen
 from ui.field.input import FDInput
 from ui.field.button import FDRectangleButton
 from service.server.auth import auth_user
+from validators import register as RegisterValidator
 
 
-class AuthScreen(BaseScreen):
+class AuthScreen(BaseAuthScreen):
     ''' Страница авторизации '''
 
     name = 'auth'
@@ -18,19 +19,41 @@ class AuthScreen(BaseScreen):
 
     def fill_elements(self) -> None:
         ''' Заполнить контент формой авторизации '''
-        login_field = FDInput(hint_text='Логин')
-        self.add_content(login_field)
+        self._login_field = FDInput(hint_text='Логин')
+        self._login_field.validators.extend([
+           RegisterValidator.EmptyFieldValidator(
+                self._login_field,
+                message='Поле не может быть пустым'
+            ),
+        ])
+        self.add_content(self._login_field)
 
-        password_field = FDInput(hint_text='Пароль')
-        self.add_content(password_field)
+        self._password_field = FDInput(hint_text='Пароль')
+        self._password_field.validators.extend([
+            RegisterValidator.EmptyFieldValidator(
+                self._password_field,
+                message='Поле не может быть пустым'
+            ),
+        ])
+        self.add_content(self._password_field)
 
-        submit = FDRectangleButton(title='Войти')
-        submit.bind_btn(callback=lambda: auth_user(login=login_field.get_value(),
-                                                   password=password_field.get_value()))
-        self.add_content(submit)
+        self._submit_btn = FDRectangleButton(title='Войти')
+        self._submit_btn.bind_btn(callback=self.submit)
+        self.add_content(self._submit_btn)
 
-        register = FDRectangleButton(title='Регистрация')
-        register.bind_btn(callback=lambda: self._path_manager.forward('register'))
-        self.add_content(register)
+        self._register_btn = FDRectangleButton(title='Регистрация')
+        self._register_btn.bind_btn(callback=lambda: self._path_manager.forward('register'))
+        self.add_content(self._register_btn)
 
         self.add_content(MDBoxLayout())
+
+    def is_valid(self) -> bool:
+        ''' Валидация формы '''
+        form_fields = (self._login_field, self._password_field,)
+        return all(map(lambda f: not f.error, form_fields))
+
+    def submit(self) -> None:
+        ''' Проверить и отправить форму на сервер '''
+
+        if not self.is_valid():
+            self.show_error_message('Форма не заполнена!')
