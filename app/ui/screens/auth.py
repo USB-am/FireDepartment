dd670a80fc150260a8c0f715d642e8ff1686d6d0
USB-am <smit.acer@mail.ru>
2026-01-09 05:35:25 +0300

    Created functional for auth user

--- app/ui/screens/auth.py
+++ app/ui/screens/auth.py
@@ -1,10 +1,37 @@
+from typing import Optional
+
+import requests
 from kivymd.uix.boxlayout import MDBoxLayout
 
 from .base import BaseAuthScreen
 from ui.field.input import FDInput
 from ui.field.button import FDRectangleButton
-from service.server.auth import auth_user
+from service.server.auth import login as LoginUser
+from service.server.secret_key import SecretKey
 from validators import register as RegisterValidator
+
+
+def send_login_request(login: str, password: str) -> Optional[requests.Response]:
+    '''
+    Отправить запрос авторизации на сервер
+
+    :param login: email пользователя
+    :param password: пароль пользователя
+    :returns requests.Response: {'status_code': 200,
+                                 'detail': 'User is login',
+                                 'secret_key': <user_secret_key>}
+    '''
+    try:
+        response = LoginUser(login, password)
+        return response
+    except requests.exceptions.ConnectionError:
+        return None
+
+
+def save_secret_key(secret_key: str) -> None:
+    ''' Сохранить secret_key в файл '''
+    secret_key_manager = SecretKey()
+    secret_key_manager.value = secret_key
 
 
 class AuthScreen(BaseAuthScreen):
@@ -57,3 +84,23 @@ class AuthScreen(BaseAuthScreen):
 
         if not self.is_valid():
             self.show_error_message('Форма не заполнена!')
+            return
+
+        res = send_login_request(
+            login=self._login_field.get_value(),
+            password=self._password_field.get_value())
+
+        if res is None:
+            self.show_error_message('Не удалось утановить соединение с сервером!')
+            return
+
+        if res.status_code == 200:
+            secret_key = res.json().get('secret_key')
+            save_secret_key(secret_key)
+            self.show_info_message('Вы авторизировались!')
+            self._path_manager.forward('main')
+        else:
+            error_detail = res.json().get('detail') if hasattr(res, 'json') \
+                else 'Возникла ошибка обработки ответа сервера!'
+            error_message = f'[{res.status_code}] {error_detail}'
+            self.show_error_message(error_message)
