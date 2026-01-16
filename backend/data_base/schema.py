57ba5f0479931a5f39f23ffd0c5406aadc30a2f8
USB-am <smit.acer@mail.ru>
2026-01-16 23:16:56 +0300

    Created schemas for all DB models

--- backend/data_base/schema.py
+++ backend/data_base/schema.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import datetime
+from typing import Optional, List
 
 from pydantic import BaseModel
 
@@ -32,3 +33,74 @@ class InfoResponse(BaseModel):
     user_id: int
     username: str
     timestamp: str
+
+
+class Tag(BaseModel):
+    id: int
+    title: str
+    emergencies: List['Emergency']
+
+
+class Short(BaseModel):
+    id: int
+    title: str
+    explanation: Optional[str]
+    into_new_line: bool
+    emergencies: List['Emergency']
+
+
+class Rank(BaseModel):
+    id: int
+    title: str
+    priority: int
+    humans: List['Human']
+
+
+class Position(BaseModel):
+    id: int
+    title: str
+    humans: List['Human']
+
+
+class Worktype(BaseModel):
+    id: int
+    title: str
+    start_work_day: datetime.date
+    finish_work_day: datetime.date
+    work_day_range: int
+    week_day_range: int
+    humans: List['Human']
+
+
+class Human(BaseModel):
+    id: int
+    title: str
+    phone_1: Optional[str]
+    phone_2: Optional[str]
+    is_firefigher: bool
+    work_day: datetime.date
+    start_vacation: datetime.date
+    finish_vacation: datetime.date
+    worktype: 'Worktype'
+    position: 'Position'
+    rank: 'Rank'
+    emergencies: List['Emergency']
+
+
+class Emergency(BaseModel):
+    id: int
+    title: str
+    description: str
+    urgent: bool
+    tags: List['Tag']
+    humans: List['Human']
+    shorts: List['Short']
+    calls: List['Calls']
+
+
+class Calls(BaseModel):
+    id: int
+    start: datetime.date
+    finish: datetime.date
+    emergency: 'Emergency'
+    info: Optional[str]
