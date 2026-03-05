2a8e1758c3c8984b52cd2388db26c770aba0d090
USB-am <smit.acer@mail.ru>
2026-03-05 23:33:23 +0300

    Created /create, /update and /delete routes for Position, Rank, Short and Tag models

--- backend/data_base/schema.py
+++ backend/data_base/schema.py
@@ -72,10 +72,33 @@ class ShortResponse(BaseModel):
     into_new_line: bool
 
 
+class CreateShortRequest(BaseModel):
+    title: str
+    explanation: Optional[str]
+    into_new_line: bool
+    emergencies_ids: Optional[List[int]]
+
+
+class UpdateShortRequest(BaseModel):
+    short_id: int
+    fields: Dict[str, Any]
+
+
 class RankResponse(BaseModel):
     id: int
     title: str
     priority: int
+
+
+class CreateRankRequest(BaseModel):
+    title: str
+    priority: int
+    humans_ids: Optional[List[int]]
+
+
+class UpdateRankRequest(BaseModel):
+    rank_id: int
+    fields: Dict[str, Any]
 
 
 class PositionResponse(BaseModel):
@@ -86,6 +109,11 @@ class PositionResponse(BaseModel):
 class CreatePositionRequest(BaseModel):
     title: str
     humans_ids: Optional[List[int]]
+
+
+class UpdatePositionRequest(BaseModel):
+    position_id: int
+    fields: Dict[str, Any]
 
 
 class WorktypeResponse(BaseModel):
--- backend/routes/models/shorts.py
+++ backend/routes/models/shorts.py
@@ -1,17 +1,72 @@
-from fastapi import APIRouter, Depends, HTTPException
-from annotated_types import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.future import select
+from sqlalchemy import update, delete
 
-from data_base.schema import ShortResponse
+from auth import TSession
+from data_base.schema import (
+    ShortResponse,
+    CreateShortRequest,
+    UpdateShortRequest)
 from data_base.session import get_session
-from data_base.models import Short
+from data_base.models import Short, Emergency
 
 
 shorts_router = APIRouter(prefix='/shorts', tags=['Shorts',])
 
 
-TSession = Annotated[AsyncSession, Depends(get_session)]
+@shorts_router.post('/create', response_model=ShortResponse, status_code=status.HTTP_201_CREATED)
+async def create_short(form: CreateShortRequest, session: TSession) -> ShortResponse:
+    title = form.title
+    stmt = select(Short).filter_by(title=title)
+    result = await session.execute(stmt)
+    short = result.scalars().first()
+
+    if short is not None:
+        raise HTTPException(
+            status_code=409,
+            detail=f'Short.title=`{title}` is already exists.'
+        )
+
+    emergencies_ids = form.emergencies_ids
+    stmt = select(Emergency).where(Emergency.id.in_(emergencies_ids))
+    result = await session.execute(stmt)
+    emergencies = result.scalars().all()
+
+    explanation = form.explanation
+    into_new_line = form.into_new_line
+
+    new_short = Short(
+        title=title,
+        explanation=explanation if explanation else title,
+        into_new_line=into_new_line,
+        emergencies=emergencies
+    )
+    session.add(new_short)
+    await session.commit()
+
+    return ShortResponse(
+        id=new_short.id,
+        title=new_short.title,
+        explanation=new_short.explanation,
+        into_new_line=new_short.into_new_line,
+    )
+
+
+@shorts_router.put('/update/{short_id}', status_code=status.HTTP_201_CREATED)
+async def update_short(form: UpdateShortRequest, session: TSession) -> None:
+    short_id = form.short_id
+    fields = form.fields
+
+    stmt = update(Short).where(Short.id==short_id).values(**fields)
+    await session.execute(stmt)
+    await session.commit()
+
+
+@shorts_router.delete('/delete/{short_id}', status_code=status.HTTP_200_OK)
+async def delete_short(short_id: int, session: TSession) -> None:
+    stmt = delete(Short).where(Short.id==short_id)
+    await session.execute(stmt)
+    await session.commit()
 
 
 @shorts_router.get('/{short_id}', response_model=ShortResponse)
