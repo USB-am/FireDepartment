a9fcee7881f6196f2f12551226d9828d81a4600d
USB-am <smit.acer@mail.ru>
2026-07-12 15:51:10 +0300

    Finished users endpoints

--- backend/api/v1/endpoints/users.py
+++ backend/api/v1/endpoints/users.py
@@ -1,5 +1,6 @@
 import hashlib
 from datetime import datetime, timedelta
+from typing import Tuple
 
 import bcrypt
 from fastapi import APIRouter, HTTPException, Request, Response
@@ -15,6 +16,45 @@ from models.user import User, RefreshToken, UserProfile
 
 users_router = APIRouter(prefix='/users', tags=['Users',])
 
+# TODO: сделать не магические цифры
+ACCESS_TOKEN_MAX_AGE = 15*60
+REFRESH_TOKEN_MAX_AGE = 7*24*60*60
+
+
+def set_token_cookies_to_response(access_token: str, refresh_token: str, response: Response) -> Response:
+    response.set_cookie(
+        key='access_token',
+        value=access_token,
+        httponly=True,
+        secure=SECURE,
+        samesite='lax',
+        max_age=ACCESS_TOKEN_MAX_AGE)
+    response.set_cookie(
+        key='refresh_token',
+        value=refresh_token,
+        httponly=True,
+        secure=SECURE,
+        samesite='lax',
+        max_age=REFRESH_TOKEN_MAX_AGE)
+    return response
+
+
+def create_access_token(user_email: str) -> str:
+    return auth.create_access_token(uid=user_email)
+
+
+def create_refresh_token(user: User) -> Tuple[str, RefreshToken]:
+    refresh_token_value = auth.create_refresh_token(uid=user.email)
+    sha_hash = hashlib.sha256(refresh_token_value.encode('utf-8')).hexdigest()
+    refresh_token = RefreshToken(
+        user_id=user.id,
+        token_hash=sha_hash,
+        expires_at=datetime.utcnow() + timedelta(days=7),
+        created_at=datetime.utcnow(),
+        revoked=False)
+
+    return (refresh_token_value, refresh_token)
+
 
 @users_router.post('/login', response_model=UserResponse)
 async def login_user(form: UserLoginRequest, session: TSession, response: Response) -> UserResponse:
@@ -25,8 +65,26 @@ async def login_user(form: UserLoginRequest, session: TSession, response: Respon
     if user is None:
         raise HTTPException(
             status_code=404,
-            detail=f'User.email="{email}" is not already exists!'
+            detail=f'User.email="{form.email}" is not already exists!'
         )
+
+    if not bcrypt.checkpw(form.password.encode('utf-8'), user.password_hash.encode('utf-8')):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid password"
+        )
+
+    access_token = create_access_token(user.email)
+    plain_refresh, refresh_token = create_refresh_token(user)
+
+    session.add(refresh_token)
+    await session.commit()
+
+    response = set_token_cookies_to_response(access_token, plain_refresh, response)
+    return UserResponse(
+        id=user.id,
+        email=user.email,
+        username=user.username)
 
 
 @users_router.post('/refresh')
@@ -44,38 +102,23 @@ async def refresh_access(request: Request, session: TSession, response: Response
         RefreshToken.revoked==False,
         RefreshToken.expires_at>datetime.utcnow())
     result = await session.execute(stmt)
-    refresh_token = result.scalars().first()
+    old_refresh_token = result.scalars().first()
 
-    user = refresh_token.user
+    if old_refresh_token is None:
+        raise HTTPException(
+            status_code=401,
+            detail=f'Not found actual refresh_token.'
+        )
+    user = old_refresh_token.user
 
-    access_token = auth.create_access_token(uid=user.email)
-    response.set_cookie(
-        key='access_token',
-        value=access_token,
-        httponly=True,
-        secure=SECURE,
-        samesite='lax',
-        max_age=15*60)
+    access_token = create_access_token(user.email)
+    plain_refresh, refresh_token = create_refresh_token(user)
+    old_refresh_token.revoked = True
 
-    refresh_token_value = auth.create_refresh_token(uid=user.email)
-    sha_hash = hashlib.sha256(refresh_token_value.encode('utf-8')).hexdigest()
-    refresh_token = RefreshToken(
-        user_id=user.id,
-        token_hash=sha_hash,
-        expires_at=datetime.utcnow() + timedelta(days=7),
-        created_at=datetime.utcnow(),
-        revoked=False)
     session.add(refresh_token)
     await session.commit()
 
-    response.set_cookie(
-        key='refresh_token',
-        value=refresh_token,
-        httponly=True,
-        secure=SECURE,
-        samesite='lax',
-        max_age=7*24*60*60)
-
+    response = set_token_cookies_to_response(access_token, plain_refresh, response)
     return {'status_code': 200, 'detail': 'Access-token is updated.'}
 
 
@@ -101,40 +144,16 @@ async def create_user(form: UserRegisterRequest, session: TSession, response: Re
     session.add(new_user)
     await session.flush()
 
-    refresh_token_value = auth.create_refresh_token(uid=new_user.email)
-    # sha_hash = hashlib.sha256(refresh_token_value.encode('utf-8')).digest()
-    sha_hash = hashlib.sha256(refresh_token_value.encode('utf-8')).hexdigest()
-    # token_hash = bcrypt.hashpw(sha_hash, bcrypt.gensalt()).decode('utf-8')
-    refresh_token = RefreshToken(
-        user_id=new_user.id,
-        token_hash=sha_hash,
-        expires_at=datetime.utcnow() + timedelta(days=7),
-        created_at=datetime.utcnow(),
-        revoked=False)
-    session.add(refresh_token)
-
     profile = UserProfile(user_id=new_user.id)
     session.add(profile)
 
+    access_token = create_access_token(email)
+    plain_refresh, refresh_token = create_refresh_token(new_user)
+    session.add(refresh_token)
+
     await session.commit()
 
-    access_token = auth.create_access_token(uid=new_user.email)
-
-    response.set_cookie(
-        key='access_token',
-        value=access_token,
-        httponly=True,
-        secure=SECURE,
-        samesite='lax',
-        max_age=15*60)  # TODO: сделать не магические цифры
-    response.set_cookie(
-        key='refresh_token',
-        value=refresh_token_value,
-        httponly=True,
-        secure=SECURE,
-        samesite='lax',
-        max_age=7*24*60*60)  # TODO: сделать не магические цифры
-
+    response = set_token_cookies_to_response(access_token, plain_refresh, response)
     return UserResponse(
         id=new_user.id,
         email=new_user.email,
