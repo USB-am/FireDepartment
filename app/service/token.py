56965f85765cdb6a66182c6b6adecf4360f1bee7
USB-am <smit.acer@mail.ru>
2026-08-15 02:06:25 +0300

    Updated token manager class

--- app/service/token.py
+++ app/service/token.py
@@ -2,30 +2,33 @@ import os
 import json
 from typing import Optional
 
+from config import BASE_DIR
 from exceptions import NoTokenError, NotFoundTokenFileError
 
 
-class AccessTokenManager:
-    def __init__(self, path_to_token: str=None):
-        if path_to_token is None:
-            path_to_token = os.getcwd()
-
-        self._path_to_token = os.path.join(path_to_token, 'temp_token.json')
+class AccessToken:
+    def __init__(self, path_to_token: str|None=None):
+        self._path_to_token = BASE_DIR if path_to_token is None else path_to_token
 
     def save_token(self, access_token: str, refresh_token: str) -> None:
         with open(self._path_to_token, mode='w') as tmp_token_file:
-            json_data = {'access_token': access_token, 'refresh_token': refresh_token}
-            json.dump(json_data, tmp_token_file, indent=2)
+            json.dump(
+                {'access_token': access_token,
+                 'refresh_token': refresh_token
+                },
+                tmp_token_file
+            )
 
     def get_token(self, key: str='access_token') -> str:
         try:
             with open(self._path_to_token, mode='r') as tmp_token_file:
-                json_data = json.load(tmp_token_file)
+                token_data = json.load(tmp_token_file)
 
                 try:
-                    return json_data[key]
+                    return token_data[key]
+
                 except KeyError:
-                    raise NoTokenError('User access-token not found error!')
+                    raise NoTokenError(f'User.{key} not found error!')
 
         except FileNotFoundError:
             raise NotFoundTokenFileError('Token file not found error!')
